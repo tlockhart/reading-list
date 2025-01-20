fef71abcc7ff253a691f3f8b29b1b2ff335925cf
Tony Lockhart <tony.lockhart@ymail.com>
2025-01-20 13:45:43 -0500

    chore: created client/build

---
+++ client/build/precache-manifest.f6e91e4af8818be76034c7f2d5dba458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9765ce707d2989267f1",
+    "url": "/static/css/main.1a7cdacc.chunk.css"
+  },
+  {
+    "revision": "b9765ce707d2989267f1",
+    "url": "/static/js/main.b9765ce7.chunk.js"
+  },
+  {
+    "revision": "e2a597fa7b572dee1540",
+    "url": "/static/js/1.e2a597fa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a14253f35468c5b306ba8c5893033a6",
+    "url": "/index.html"
+  }
+];
